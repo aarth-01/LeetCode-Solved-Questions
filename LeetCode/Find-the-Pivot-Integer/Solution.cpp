71b5da0ca07935da3c599fc2f1af290e653cf4cc
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-02-19 12:19:22 +0530

    Add solution for Find the Pivot Integer

---
+++ LeetCode/Find-the-Pivot-Integer/Solution.cpp
@@ -0,0 +1,19 @@
+1class Solution {
+2public:
+3    int pivotInteger(int n) {
+4        int mid = n/2,sum1=0, sum2=0;
+5        if(n ==1 ) return 1;
+6
+7
+8        while(mid<n){
+9            if(sum1>sum2) return -1;
+10
+11            long long sum1 = (mid*(mid+1))/2;
+12            long long sum2 = (n*(n+1))/2 - (mid*(mid-1))/2;
+13
+14            if(sum1 == sum2) return mid;
+15            mid++;
+16        }
+17        return -1;
+18    }
+19};
