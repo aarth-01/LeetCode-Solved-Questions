060413d950c66d730161dee13341a32bb552fe31
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-04-09 23:52:32 +0530

    Time: 0 ms (100%), Space: 14.1 MB (10.37%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>freq;
+
+        for(auto it:nums){
+            freq[it]++;
+        }
+        for(auto it:freq){
+            if(it.second==1) return it.first;
+        }
+        return 0;
+    }
+};
