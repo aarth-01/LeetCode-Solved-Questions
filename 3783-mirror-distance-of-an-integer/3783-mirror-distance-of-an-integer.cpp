0f484f252523db271527c89fe8ab981ffc4fa6e3
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-04-19 01:26:19 +0530

    Time: 1 ms (8.93%), Space: 8.5 MB (86.34%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int rev=0,temp=n;
+
+        while(n>0){
+            int dig= n%10;
+            rev = rev*10 + dig;
+            n/=10;
+        }
+
+        return abs(rev-temp);
+    }
+};
