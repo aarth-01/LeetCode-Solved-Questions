60ffd2874647e0e998ce53f43448fdcf0611d731
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-07-15 19:44:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub - Time: O(?), Space: O(?)

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -3,10 +3,8 @@ public:
     int gcdOfOddEvenSums(int n) {
         int even_sum = n*(n+1), odd_sum = n*n;
 
-        for(int i=odd_sum; i>1; i--){
-            if((odd_sum%i==0) && (even_sum%i==0)) return i;
-        }
-        return 1;
+        
+        return gcd(even_sum, odd_sum);
     }
 };
 
