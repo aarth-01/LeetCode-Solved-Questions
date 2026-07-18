ace7d15f98bbcd5843e804b33d9f4b99412f817b
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-07-19 02:46:03 +0530

    Time: 0 ms (100%), Space: 11.1 MB (58.41%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* temp = head;
+
+
+        while(temp!=nullptr && temp->next!=nullptr){
+            swap(temp->val, (temp->next)->val);
+
+            temp = temp->next;
+            if(temp->next==nullptr || (temp->next)->next==nullptr) break;
+            else temp = temp-> next;
+        }
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
