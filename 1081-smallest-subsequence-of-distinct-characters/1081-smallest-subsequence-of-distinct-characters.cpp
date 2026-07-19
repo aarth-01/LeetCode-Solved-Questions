9de628674f836d84bce65b50937a838d4b86a50c
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-07-20 04:29:56 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.99%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> freq(26, 0);
+        vector<bool> seen(26, false);
+
+        for (char& x : s) {
+            freq[x - 'a']++;
+        }
+
+        string res = "";
+        for (char& x : s) {
+            freq[x - 'a']--;
+
+            if (seen[x - 'a'])
+                continue;
+
+            while (!res.empty() && res.back() > x &&
+                   freq[res.back() - 'a'] > 0) {
+                seen[res.back() - 'a'] = false;
+                res.pop_back();
+            }
+
+            res += x;
+            seen[x - 'a'] = true;
+        }
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
