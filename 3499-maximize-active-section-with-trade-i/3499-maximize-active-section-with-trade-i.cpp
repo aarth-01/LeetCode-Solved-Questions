2719fb188d7624a1662eb4b215825d34587b8b16
Aarth Vajandar <aarthvajandar01@gmail.com>
2026-07-22 01:38:11 +0530

    Time: 43 ms (88.39%), Space: 54.9 MB (58.04%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.size();
+        int prevZero = 0, currZero = 0;
+        int totalOnes = 0;
+        int best = 0;
+        int i = 0;
+
+        while (i < n) {
+            if (s[i] == '0') {
+                prevZero++;
+                i++;
+            } else {
+                while (i < n && s[i] == '1') {
+                    totalOnes++;
+                    i++;
+                }
+
+                while (i < n && s[i] == '0') {
+                    currZero++;
+                    i++;
+                }
+
+                if (prevZero && currZero)
+                    best = max(best, prevZero + currZero);
+
+                prevZero = currZero;
+                currZero = 0;
+            }
+        }
+
+        return totalOnes + best;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
